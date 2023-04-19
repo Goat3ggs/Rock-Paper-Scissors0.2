8ea2b91917543e7a73785778fd4b8f945ab511c8
Goat3ggs <106019655+Goat3ggs@users.noreply.github.com>
2023-04-19 16:19:04 +0300

    checkWinner

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ function getComputerChoice(){
   return choice;
 }
 
+function checkWinner(playerSelection, computerSelection) {
+  if(playerSelection === computerSelection) {
+    return 'Tie';
+  }else if((playerSelection === 'rock' && computerSelection === 'scissors') || 
+  (playerSelection === 'scissors' && computerSelection === 'paper') || 
+  (playerSelection === 'paper' && computerSelection === 'rock')) {
+    return 'Player';
+  } else {
+    return 'Computer';
+  }
+}
+
 function playRound(playerSelection, computerSelection) {
   const result = checkWinner(playerSelection, computerSelection);
   if(result === 'Tie'){
