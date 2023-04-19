0d8f5b764d5ca774a02a22dcf56c1c3c36835bb2
Goat3ggs <106019655+Goat3ggs@users.noreply.github.com>
2023-04-19 16:20:40 +0300

    update rest of the game

--- index.js
+++ index.js
@@ -27,3 +27,47 @@ function playRound(playerSelection, computerSelection) {
     return `You Lose! ${computerSelection} beats ${playerSelection}.`;
   }
 }
+
+function getPlayerChoice(){
+  let validatedInput = false;
+  while(validatedInput === false) {
+    const choice = prompt('Rock Paper Scissors');
+    if(choice == null) {
+      continue;
+    }
+    const choiceInLower = choice.toLocaleLowerCase();
+    if(options.includes(choiceInLower)){
+      validatedInput = true;
+      return choiceInLower;
+    }
+  }
+}
+
+function game(){
+  let scorePlayer = 0;
+  let scoreComputer = 0;
+  console.log('Welcome!');
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("------------");
+    if(checkWinner(playerSelection, computerSelection) === 'Player') {
+      scorePlayer++;
+    } else if(checkWinner(playerSelection,computerSelection) === 'Computer'){
+      scoreComputer++;
+    }
+  }
+  console.log('Game Over');
+  if(scorePlayer > scoreComputer){
+    console.log('Player is the winner!');
+  }
+  else if(scorePlayer < scoreComputer) {
+    console.log('Computer is the winner!');
+  }
+  else {
+    console.log('We have a tie!');
+  }
+}
+
+game();
